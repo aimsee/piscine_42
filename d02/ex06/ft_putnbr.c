3b928923dc9713324d034d3a25001abb9e43c5e4
aimsee <matt.costa@laposte.net>
2019-11-21 17:14:07 +0100

    une fonction qui affiche un nombre passé en paramètre. La fonction devra être capable d’afficher la totalité des valeurs possibles dans une variable de type int

--- d02/ex06/ft_putnbr.c
+++ d02/ex06/ft_putnbr.c
@@ -1,16 +1,12 @@
 #include <unistd.h>
 
 int	ft_putchar(char c)
-{
-	write(1, &c, 1);
-	return (0);
-}
 
-int	ft_putnbr(int nb)
+int	ftputnbr(int nb);
 {
 	unsigned int	nbr;
 
-	if (nb <0)
+	if (nb < 0)
 	{
 		ft_putchar('-');
 		nbr = nb * -1;
@@ -19,13 +15,7 @@ int	ft_putnbr(int nb)
 		nbr = nb;
 	if (nbr >= 10)
 	{
-		ft_putnbr(nbr /10);
+		ft_putnbr(nbr / 10)
 	}
 	ft_putchar(nbr % 10 + 48);
-}
-
-int	main()
-{
-	ft_putnbr(42);
-	return (0);
 }
